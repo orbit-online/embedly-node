4642a8b7bf2c2fbea95bb3bc3e73a35ec2eb38ba
Brian Haven Kejlberg <bkc@orbit.online>
2025-05-01 13:57:17 +0200

    actions: setup workflows

---
+++ tools/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# The github build action uses the official nodejs container does not have bash
+# so this script must be POSIX sh compliant
+
+set -e
+cd "$(dirname "$0")/.."
+PATH=$PWD/node_modules/.bin:$PATH
+export LOGFORMAT=cli
+
+main() {
+  rm -Rf dist/
+  mkdir -p dist/
+  mocha
+  cp -r index.js LICENSE bin/ dist/
+}
+
+main "$@"
